0a501db01aee47d9f00825f596a1cc48171aa308
Mastermehan <mastermehan1984@gmail.com>
2021-09-13 21:40:46 +0300

    Добавлено try catch

--- Calculator.java
+++ Calculator.java
@@ -29,13 +29,18 @@ public class Calculator {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        int c = calc.devide.apply(a, b);
+
+        try {
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Ошибка! Нельзя делить на ноль!");
+        }
         int d = calc.multiply.apply(2, 4);
         int e = calc.pow.apply(3);
         int f = calc.abs.apply(4);
         boolean g = calc.isPositive.test(3);
 
-        calc.println.accept(c);
         calc.println.accept(d);
         calc.println.accept(e);
         calc.println.accept(f);
